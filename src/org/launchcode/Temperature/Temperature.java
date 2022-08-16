2b16644f486c8999c15843aaa616efd6168988af
unknown <nunezfrancisco400@gmail.com>
2022-08-16 19:07:06 -0400

    java-web-dev--exceptions

--- src/org/launchcode/Temperature/Temperature.java
+++ src/org/launchcode/Temperature/Temperature.java
@@ -1,6 +1,6 @@
 package org.launchcode.Temperature;
 
-public class Temperature {
+public class Temperature  {
 
     private double fahrenheit;
 
@@ -13,9 +13,16 @@ public class Temperature {
         double absoluteZeroFahrenheit = -459.67;
 
         if (aFahrenheit < absoluteZeroFahrenheit) {
-            System.out.println("Value is below absolute zero");
+            try{
+                throw new TemperatureException("That temperature is too low!");
+            }catch (TemperatureException e){
+                e.printStackTrace();
+            }
         }
 
         fahrenheit = aFahrenheit;
     }
 }
+
+
+
--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Test out your Divide() function!
-
+        //Divide(12,0);
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
@@ -16,16 +16,45 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        System.out.println(CheckFileExtension(studentFiles.get("Carl")));
+        System.out.println(CheckFileExtension(studentFiles.get("Brad")));
+        System.out.println(CheckFileExtension(studentFiles.get("Stefanie")));
+
     }
 
     public static void Divide(int x, int y)
     {
         // Write code here!
+        if (y == 0){
+            try {
+                throw new ArithmeticException("You can not divide by 0, [derogatory term]!");
+            } catch (ArithmeticException err){
+                err.printStackTrace();
+            }
+        }else{
+            System.out.println(String.format("The quotient of %s divided by %s is: %s", x, y, x/y));
+
+        }
+
     }
 
     public static int CheckFileExtension(String fileName)
     {
+        int result = 0;
         // Write code here!
+        if(fileName == null) {
+            try {
+                throw new NullPointerException("Ile cannot be null");
+            }catch (NullPointerException error){
+                error.printStackTrace();
+                result = -1;
+                return result;
+            }
+        } else if (fileName.endsWith(".java")) {
+            result = 1;
+            return result;
+        }
+        return result;
     }
 
 }
